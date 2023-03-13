de64c2f3d15fc231bc81ca32d025caa758548a8d
Crowdbotics <team@crowdbotics.com>
2023-03-13 18:28:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "notepad-39351",
+  url: "https://notepad-39351.botics.co",
+  api: "https://notepad-39351.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
