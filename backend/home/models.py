72849a6be677e9633f03ea515b15a528c93d2b45
crowdbotics-apps <team@crowdbotics.com>
2023-06-01 22:02:08 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Test(models.Model):
+    'Generated Model'
+    location = models.BigIntegerField()
+    name = models.BigIntegerField()
