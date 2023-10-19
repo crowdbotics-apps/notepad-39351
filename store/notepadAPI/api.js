87f0db97081111464e309891ca6342cb94e8859e
Crowdbotics <engineering@crowdbotics.com>
2023-10-19 22:55:27 +0000

    Update store.

--- store/notepadAPI/api.js
+++ store/notepadAPI/api.js
@@ -7,31 +7,31 @@ function api_docs_schema_retrieve(payload) {
   return notepadAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
 }
 function api_v1_login_create(payload) {
-  return notepadAPI.post(`/api/v1/login/`, payload.data)
+  return notepadAPI.post(`/api/v1/login/`, payload)
 }
 function api_v1_signup_create(payload) {
-  return notepadAPI.post(`/api/v1/signup/`, payload.data)
+  return notepadAPI.post(`/api/v1/signup/`, payload)
 }
 function api_v1_test_list(payload) {
   return notepadAPI.get(`/api/v1/test/`)
 }
 function api_v1_test_create(payload) {
-  return notepadAPI.post(`/api/v1/test/`, payload.data)
+  return notepadAPI.post(`/api/v1/test/`, payload)
 }
 function api_v1_test_retrieve(payload) {
   return notepadAPI.get(`/api/v1/test/${payload.id}/`)
 }
 function api_v1_test_update(payload) {
-  return notepadAPI.put(`/api/v1/test/${payload.id}/`, payload.data)
+  return notepadAPI.put(`/api/v1/test/${payload.id}/`, payload)
 }
 function api_v1_test_partial_update(payload) {
-  return notepadAPI.patch(`/api/v1/test/${payload.id}/`, payload.data)
+  return notepadAPI.patch(`/api/v1/test/${payload.id}/`, payload)
 }
 function api_v1_test_destroy(payload) {
   return notepadAPI.delete(`/api/v1/test/${payload.id}/`)
 }
 function rest_auth_login_create(payload) {
-  return notepadAPI.post(`/rest-auth/login/`, payload.data)
+  return notepadAPI.post(`/rest-auth/login/`, payload)
 }
 function rest_auth_logout_retrieve(payload) {
   return notepadAPI.get(`/rest-auth/logout/`)
@@ -40,28 +40,28 @@ function rest_auth_logout_create(payload) {
   return notepadAPI.post(`/rest-auth/logout/`)
 }
 function rest_auth_password_change_create(payload) {
-  return notepadAPI.post(`/rest-auth/password/change/`, payload.data)
+  return notepadAPI.post(`/rest-auth/password/change/`, payload)
 }
 function rest_auth_password_reset_create(payload) {
-  return notepadAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return notepadAPI.post(`/rest-auth/password/reset/`, payload)
 }
 function rest_auth_password_reset_confirm_create(payload) {
-  return notepadAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return notepadAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
 function rest_auth_registration_create(payload) {
-  return notepadAPI.post(`/rest-auth/registration/`, payload.data)
+  return notepadAPI.post(`/rest-auth/registration/`, payload)
 }
 function rest_auth_registration_verify_email_create(payload) {
-  return notepadAPI.post(`/rest-auth/registration/verify-email/`, payload.data)
+  return notepadAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
 function rest_auth_user_retrieve(payload) {
   return notepadAPI.get(`/rest-auth/user/`)
 }
 function rest_auth_user_update(payload) {
-  return notepadAPI.put(`/rest-auth/user/`, payload.data)
+  return notepadAPI.put(`/rest-auth/user/`, payload)
 }
 function rest_auth_user_partial_update(payload) {
-  return notepadAPI.patch(`/rest-auth/user/`, payload.data)
+  return notepadAPI.patch(`/rest-auth/user/`, payload)
 }
 export const apiService = {
   api_docs_schema_retrieve,
